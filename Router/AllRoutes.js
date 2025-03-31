d7f1d13892a24168d873f94892e613dc103a1141
Ultimate35000 <njorez@yahoo.fr>
2025-03-31 15:08:15 +0200

    Add connexion page

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -5,7 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/lacarte", "La Carte", "/pages/lacarte.html"),
-
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
