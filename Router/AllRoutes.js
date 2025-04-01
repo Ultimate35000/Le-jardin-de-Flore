d0c89e49c4d0f78097c0430ebf8e3ff6db4df380
Ultimate35000 <njorez@yahoo.fr>
2025-04-01 17:11:08 +0200

    Modification maj

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/lacarte", "La Carte", "/pages/lacarte.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/edithPassword.html"),    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
