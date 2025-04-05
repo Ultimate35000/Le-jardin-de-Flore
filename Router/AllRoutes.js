c0628cdbbb40cbf8c206e170bb058349323aeb6b
Ultimate35000 <njorez@yahoo.fr>
2025-04-05 16:47:08 +0200

    add JS au signup

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -5,11 +5,12 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/lacarte", "La Carte", "/pages/lacarte.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/edithPassword.html"),
-    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),    
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"), 
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),   
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
