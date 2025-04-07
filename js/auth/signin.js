282328ce0b65e65eccf20333925d4a803587a1a2
Ultimate35000 <njorez@yahoo.fr>
2025-04-07 11:11:37 +0200

    add js connexion

---
+++ js/auth/signin.js
@@ -0,0 +1,21 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        alert("Vous êtes connecté");
+
+        const token = "fqdfhqdfhqdfhdhqdhqdfhqfdh";
+
+        window.location.replace("/");
+
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+
+}
